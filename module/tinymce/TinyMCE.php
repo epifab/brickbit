af94389c897b88dd756855c52357977dc5b5e60a
Fabio Epifani <epifab@gmail.com>
2014-02-27 23:38:01 +0000

    tinymce 4

--- module/tinymce/TinyMCE.php
+++ module/tinymce/TinyMCE.php
@@ -3,8 +3,7 @@ namespace module\tinymce;
 
 class TinyMCE extends \system\Module {
 	public static function onRun(\system\Component $component) {
-		$component->addJs(\system\Module::getAbsPath('tinymce', '3.5.4.1/jscripts/tiny_mce/jquery.tinymce.js'));
+		$component->addJs(\system\Module::getAbsPath('tinymce', '4.0.18/tinymce.min.js'));
 		$component->addJs(\system\Module::getAbsPath('tinymce', 'main.js'));
 	}
-
 }
