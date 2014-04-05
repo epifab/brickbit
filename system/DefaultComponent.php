0f12f350a43978bd346df63449027bd263b0f10a
Fabio Epifani <epifab@gmail.com>
2014-04-05 16:51:33 +0100

    Adding access informations to the default action

--- system/DefaultComponent.php
+++ system/DefaultComponent.php
@@ -6,6 +6,10 @@ class DefaultComponent extends \system\Component {
     parent::__construct('default', 'system', 'Default', $url, array());
   }
   
+  public static function accessDefault() {
+    return true;
+  }
+  
   public function runDefault() {
     throw new exceptions\PageNotFound('Page not found');
   }
