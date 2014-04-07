dcd56c057a6a6f34c0dc90429e4839b58b18e2bf
Fabio Epifani <epifab@gmail.com>
2014-04-07 23:49:45 +0100

    Theme re-added

---
+++ theme/ciderbit2/Theme.php
@@ -0,0 +1,30 @@
+<?php
+namespace theme\ciderbit2;
+
+use system\Main;
+use system\Component;
+
+class Theme {
+  public static function preRun(Component $component) {
+    // jquery library
+    $component->addJs('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+    // jquery ajax form submission plugin
+    $component->addJs(Main::themePathRel('js/jquery.form.js'));
+    // ciderbit object
+    $component->addJs(Main::themePathRel('js/jquery.ciderbit.js'));
+  }
+  
+  public static function onRun(Component $component) {
+    // bootstrap
+    $component->addJs(Main::themePathRel('bootstrap/js/bootstrap.min.js'));
+    $component->addCss(Main::themePathRel('bootstrap/css/bootstrap.min.css'));
+
+    // jquery ui
+    $component->addJs(Main::themePathRel('js/jquery-ui-1.10.4/js/jquery-ui-1.10.4.custom.min.js'));
+    $component->addCss(Main::themePathRel('js/jquery-ui-1.10.4/css/flick/jquery-ui-1.10.4.custom.css'));
+    
+    // custom
+    $component->addJs(Main::themePathRel('js/ciderbit.js'));
+    $component->addCss(Main::themePathRel('css/theme.css'));
+  }
+}
