ca3a461fb490c5df874fb91a5ca7d94c00191a7f
Fabio Epifani <epifab@gmail.com>
2014-04-08 01:12:17 +0100

    Removing config files

--- appdata/default/config.php
+++ appdata/default/config.php
@@ -1,4 +1,31 @@
 <?php
 return array(
+  // Database connection
+  'dbUser' => '',
+  'dbPass' => '',
+  'dbHost' => '',
+  'dbName' => '',
+  'dbType' => 'mysql',
   
+  // Language
+  'defaultLang' => 'en',
+  'languages' => array('en', 'it'),
+
+  // Theme
+  'theme' => 'ciderbit2',
+  'themes' => array('ciderbit2'),
+  
+  // Base directory
+  'baseDir' => '/',
+    
+  'siteTitle' => 'CIDERbit',
+  'siteSubtitle' => 'YAF (yet another framework)',
+  
+  'defaultPateTitle' => 'CIDERbit',
+    
+  'coreCache' => false,
+  'debug' => false,
+    
+  'cacheDir' => 'appdata/default/cache/',
+  'filesDir' => 'appdata/default/files/',
 );
